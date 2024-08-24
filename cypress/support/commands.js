bbf03b3082136776e634332d277071ed1c4d7946
nicollefreitas <nicollefreitas062@gmail.com>
2024-08-24 18:39:32 -0300

    Adiciona commands personalizados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,90 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//comandos página de Login
+Cypress.Commands.add('visitLogin', () => {
+  cy.visit('https://seubarriga.wcaquino.me/login');
+});
+
+Cypress.Commands.add('typeEmail', (email) => {
+  cy.get('#email').type('0304@gmail.com');
+});
+
+Cypress.Commands.add('typeSenha', (password) => {
+  cy.get('#senha').type('123456');
+});
+
+Cypress.Commands.add('clickBotaoLogin', () => {
+  cy.get('.btn').click();
+});
+
+Cypress.Commands.add('Mensagem',() => {
+  cy.get('.alert').should('have.text', 'Bem vindo, julia!');
+});
+
+//comandos página de Criar Movimentação
+Cypress.Commands.add('visitLogin', () => {
+  cy.visit('https://seubarriga.wcaquino.me/login');
+});
+
+Cypress.Commands.add('typeEmail', (email) => {
+  cy.get('#email').type('0304@gmail.com');
+});
+
+Cypress.Commands.add('typeSenha', (password) => {
+  cy.get('#senha').type('123456');
+});
+
+Cypress.Commands.add('clickBotaoLogin', () => {
+  cy.get('.btn').click();
+});
+
+Cypress.Commands.add('clickAbaMovimentacao', () => {
+  cy.get(':nth-child(3) > a').click();
+})
+
+
+Cypress.Commands.add('typeTipo', (type) => {
+  cy.get('#tipo').type('Receita');
+});
+
+Cypress.Commands.add('typeDataTransacao', (date) => {
+  cy.get('#data_transacao').type('23/07/2024');
+});
+
+Cypress.Commands.add('typeDataPagamento', (date) => {
+  cy.get('#data_pagamento').type('23/07/2024');
+});
+
+Cypress.Commands.add('typeDescricao', (description) => {
+  cy.get('#descricao').type('salario');
+});
+
+Cypress.Commands.add('typeInteressado', (interested) => {
+  cy.get('#interessado').type('Colaborador');
+});
+
+Cypress.Commands.add('typeValor', (value) => {
+  cy.get('#valor').type('1412');
+});
+
+Cypress.Commands.add('typeValorNegativo', (value) => {
+  cy.get('#valor').type('-1412');
+});
+
+Cypress.Commands.add('selectConta', (account) => {
+  cy.get('#conta').select('Conta teste');
+});
+
+Cypress.Commands.add('clickStatusPago', () => {
+  cy.get('#status_pago').click();
+});
+
+Cypress.Commands.add('clickBotaoSalvar', () => {
+  cy.get('.btn-group').click();
+});
+
+Cypress.Commands.add('MensagemSucesso',() => {
+  cy.get('.alert').should('have.text', 'Movimentação adicionada com sucesso!');
+});
+
+Cypress.Commands.add('MensagemErro',() => {
+  cy.get('.alert').should('have.text', 'Não é possível adicionar receita negativa.');
+});
